298cfea3dd92fc9da815d98bee469101b3c542b1
Kyle <conradwebdev@gmail.com>
2020-12-29 15:41:22 -0800

    added framer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -8,10 +9,15 @@ export default function IndexPage() {
     <Layout>
       <SEO title="Home" />
       <div className="h-full w-full flex items-center">
-        <h1 className="text-gradient-util text-transparent text-32 leading-40 md:text-56 md:leading-72 lg:text-56 lg:leading-72 font-black font-headings">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ type: "tween", ease: "easeOut", duration: 0.75 }}
+          className="text-gradient-util text-transparent text-32 leading-40 md:text-56 md:leading-72 lg:text-56 lg:leading-72 font-black font-headings"
+        >
           Hi, I'm Kyle. I solve problems and design solutions to help businesses
           grow.
-        </h1>
+        </motion.h1>
       </div>
     </Layout>
   );
